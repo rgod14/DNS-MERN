4fe60fa3435e6622a54ae159a94966c50ecfee62
Yogita Khanna <yogitakhanna06@gmail.com>
2024-03-30 20:30:26 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const  express  =  require("express");
+const  cors  =  require("cors");
+const app = express();
+const mongoose = require('mongoose');
+const dns = require('./routes/Dns');
+const user = require('./routes/Users')
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+app.use(cors())
+
+
+// Middlewares
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get('/getData', (req, res) =>{
+    res.json([1,2,3,4,5])
+})
+
+app.use('/api', dns);
+app.use('/user', user);
+
+const PORT = 4545 || process.env.PORT;
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+   console.log('server started')
+})
+
+app.listen(PORT, ()=>{
+    console.log("Server starting at port: "+PORT);
+})
+
